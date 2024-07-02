0809b02047cb3c0b397e36b47c2a43b65b8a1e92
MintBics <38803684+MintBics@users.noreply.github.com>
2024-07-02 21:06:16 +1000

    Unit test framework

---
+++ CSNet-Library-Tests/UnitTests.cs
@@ -0,0 +1,13 @@
+using CSNet_LibraryManagement_DotNetCore;
+
+namespace CSNet_Library_Tests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+        }
+    }
+}
